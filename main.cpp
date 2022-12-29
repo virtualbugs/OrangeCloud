e5bcb8eb9e1af410ebc4a58160a548b504ed96bf
_caliskan_ <caliskan2mustafa@gmail.com>
2022-12-29 16:37:54 +0300

    Dark Theme is applied to UI

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowTitle("WeatherApp");
     //w.setFixedSize(200,400);
     w.show();
     return a.exec();
